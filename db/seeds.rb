14a3ec4699dc61f33a077494c538d20a0650a5d6
Julien de Lambilly <julien.delambilly@gmail.com>
2023-02-22 15:25:03 +0100

    yelp

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+dishoom = { name: "Dishoom", address: "7 Boundary St, London E2 7JE", category: "Asian" }
+pizza_east = { name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", category: "Italian" }
+brasserie = { name: "Brasserie de la Croisée", address: "Route de Lausanne, 1020 Renens", category: "French" }
+
+[dishoom, pizza_east, brasserie].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
